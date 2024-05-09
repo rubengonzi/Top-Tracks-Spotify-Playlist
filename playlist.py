2b547293ea3320cd3590ba814076554073460b25
Ruben <rubengonzalezgarcia7@gmail.com>
2024-05-09 22:01:23 +0200

    fix: styling

--- playlist.py
+++ playlist.py
@@ -1,3 +1,5 @@
+"""Module creating or updating a Spotify playlist with the users top songs."""
+
 import os
 from dotenv import load_dotenv
 import spotipy
@@ -10,15 +12,16 @@ PLAYLIST_NAME = os.getenv("PLAYLIST_NAME")
 PLAYLIST_LENGTH = os.getenv("PLAYLIST_LENGTH")
 TIME_RANGE = os.getenv("TIME_RANGE")
 
-scope = "playlist-read-private playlist-modify-private user-top-read"
+SCOPE = "playlist-read-private playlist-modify-public user-top-read"
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=SCOPE))
 
 def get_playlist():
+    """Return playlist if present else create a new one."""
     for playlist in sp.user_playlists(USERNAME)['items']:
         if playlist['name'] == PLAYLIST_NAME:
             return playlist
-    return sp.user_playlist_create(user=USERNAME, name=PLAYLIST_NAME, public=False)
+    return sp.user_playlist_create(user=USERNAME, name=PLAYLIST_NAME)
 
 top_tracks = sp.current_user_top_tracks(time_range=TIME_RANGE, limit=PLAYLIST_LENGTH)
 track_uris = [track['uri'] for track in top_tracks['items']]
