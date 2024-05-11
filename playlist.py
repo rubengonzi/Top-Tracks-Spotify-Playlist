2088742555c256f01122fea22ed874090668eea8
Ruben <rubengonzalezgarcia7@gmail.com>
2024-05-11 08:40:30 +0200

    fix: shorten line

--- playlist.py
+++ playlist.py
@@ -12,6 +12,8 @@ PLAYLIST_NAME = os.getenv("PLAYLIST_NAME")
 PLAYLIST_LENGTH = os.getenv("PLAYLIST_LENGTH")
 TIME_RANGE = os.getenv("PLAYLIST_TIME_RANGE")
 
+REPOSITORY_URL = "https://github.com/rubengonzi/Top-Tracks-Spotify-Playlist"
+
 SCOPE = "playlist-read-private playlist-modify-public user-top-read"
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=SCOPE))
@@ -21,7 +23,7 @@ def get_playlist():
     for playlist in sp.user_playlists(USERNAME)['items']:
         if playlist['name'] == PLAYLIST_NAME:
             return playlist
-    return sp.user_playlist_create(user=USERNAME, name=PLAYLIST_NAME, description="https://github.com/rubengonzi/Top-Tracks-Spotify-Playlist")
+    return sp.user_playlist_create(user=USERNAME, name=PLAYLIST_NAME, description=REPOSITORY_URL)
 
 top_tracks = sp.current_user_top_tracks(time_range=TIME_RANGE, limit=PLAYLIST_LENGTH)
 track_uris = [track['uri'] for track in top_tracks['items']]
